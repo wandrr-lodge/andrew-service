df530d77d3d769065ebcf9df43aeb3fe3137f7d7
Ben Chasin <benachasin@gmail.com>
2020-08-07 15:46:41 -0700

    added linter, webpack, and some react

--- client/app.jsx
+++ client/app.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+class App extends React.compoment {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div id="reviews-section">
+        <div id="ratings"></div>
+        <div id="latest-reviews"></div>
+      </div>
+    )
+
+  }
+}
