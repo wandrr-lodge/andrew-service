81398b406946f6ddc1ff66b7e2d634fb674546c5
Ben Chasin <benachasin@gmail.com>
2020-08-07 16:13:22 -0700

    Added skeleton of first react  component

--- client/modules/totalScore.jsx
+++ client/modules/totalScore.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
 
+class TotalScore extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div>
+      <div className="score">{props.score}</div>
+      <div className="words">
+        <div id="scoreWord">{props.word}</div>
+        <div id="totalReviews">{props.reviews}</div>
+      </div>
+    </div>
+    )
+  }
+}
