79bc11d954f9542c6f18e36e13afe020bd17060b
Ben Chasin <benachasin@gmail.com>
2020-08-10 15:39:17 -0700

    finished css for first half of default view

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TotalScore from './modules/totalScore.jsx';
-import Categories from './modules/Categories.jsx';
+import DefaultView from './modules/defaultView.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,18 +12,7 @@ class App extends React.Component {
     return (
       <div id="reviews-section">
         <div id="ratings">
-          <TotalScore score={13} word={'good'} reviews={null} />
-        </div>
-        <div>
-          <Categories scores={[
-            { name: 'Security', score: 9.4 },
-            { name: 'Location', score: 9.9 },
-            { name: 'Staff', score: 9.2 },
-            { name: 'Atmosphere', score: 8.8 },
-            { name: 'Cleanliness', score: 8.9 },
-            { name: 'Facilities', score: 8.4 },
-            { name: 'Value For Money', score: 9.1 }
-          ]} />
+          <DefaultView />
         </div>
         <div id="latest-reviews"></div>
       </div>
