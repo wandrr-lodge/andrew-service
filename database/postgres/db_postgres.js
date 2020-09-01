0ed10579ca8869b6ab9463d8f39ca1b2a7cd81d1
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-08-31 23:25:42 -0700

    added configuration options to the query

--- database/postgres/db_postgres.js
+++ database/postgres/db_postgres.js
@@ -1,21 +1,40 @@
 /* eslint-disable no-console */
 const { Client } = require('pg');
 const config = require('./db_postgres_config');
+const { getRandomInt } = require('../data/data-generator-utils');
 
 const client = new Client(config);
 
-const reviewsRecords = 5048;
+// config variables
+const records = 5048; // number of reviews
+const joinedQuery = true;
+const connectionType = 'client'; // client or pool
+const sameRecord = false; // whether the query asks for a different record each time
+
+let reviewID;
+let query;
+if (!sameRecord) {
+  const last90 = Math.floor(records * 0.9);
+  reviewID = getRandomInt(last90, records);
+} else {
+  reviewID = Math.floor(records * 0.9);
+}
+
+if (!joinedQuery) {
+  query = `SELECT * FROM reviews WHERE review_id=${reviewID}`;
+} else {
+  query = `SELECT * FROM reviews, authors, hostels
+  WHERE reviews.review_id=${reviewID}
+  AND reviews.author_id = authors.id
+  AND reviews.hostel_id = hostels.id`;
+}
 
 (async () => {
-  const reviewId = Math.floor(reviewsRecords * 0.9);
-
   // start timer
   const start = Date.now();
   await client.connect();
 
-  // determine review to query
-
-  const res = await client.query(`SELECT * FROM reviews WHERE review_id=${reviewId}`);
+  const res = await client.query(query);
   // stop timer
   const end = Date.now();
 
